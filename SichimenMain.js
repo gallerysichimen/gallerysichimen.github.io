824845a855484813c0fa1d2da955dd8508917293
gizmo-pants <gigis.coutings.quad@gmail.com>
2025-07-22 00:21:02 +0900

    あ

--- SichimenMain.js
+++ SichimenMain.js
@@ -495,17 +495,6 @@ function AchiveFolder(exhibitions){
 			GoScroll(`${exhibitions[i].year}`);
 		});
 	}
-	/*
-	for(i=0; i<clickYear.length; i++){
-		var flip_i = clickYear.length-i-1;
-		Dosomething(document.getElementById(`tag${exhibitions[clickYear[i+1]].year}`), clickYear[flip_i]*2+clickYear.indexOf(clickYear[i]))
-	}
-	function Dosomething(target,testnum) {
-		target.addEventListener('click', function() {
-			GoScroll(`achivebox`,testnum);
-		});
-	}
-	*/
 }
 	
 
